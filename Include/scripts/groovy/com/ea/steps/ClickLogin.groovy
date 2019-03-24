4dcf441770256fc19c245ee7d5410fa616e142d7
Nick <Nick@Nick-HP>
2019-03-24 19:49:07 +0000

    Added test case and feature file.

--- Include/scripts/groovy/com/ea/steps/ClickLogin.groovy
+++ Include/scripts/groovy/com/ea/steps/ClickLogin.groovy
@@ -47,22 +47,17 @@ import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 
 
-class ClickLogin {
+class LoginSteps {
 	/**
 	 * The step definitions below match with Katalon sample Gherkin steps
 	 */
-	@Given("I want to write a step with (.*)")
-	def I_want_to_write_a_step_with_name(String name) {
-		println name
-	}
-
-	@When("I check for the (\\d+) in step")
-	def I_check_for_the_value_in_step(int value) {
-		println value
-	}
-
-	@Then("I verify the (.*) in step")
-	def I_verify_the_status_in_step(String status) {
-		println status
+	@Given("I navigate to the login page")
+	def I_Navigate_To_Login_Page() {
+		WebUI.openBrowser('')
+		WebUI.navigateToUrl('https://secure.hmce.gov.uk/is2/')
+		
+		WebUI.click(findTestObject('Object Repository/Page_Welcome to Intrastat/input'))
+		
+		WebUI.click(findTestObject('Object Repository/Page_Login Page/input_Password_Quit'))
 	}
 }
